043d04b96a295968c45275284a260cacba4f25be
allansouzadk <allan.souzadk@gmail.com>
2019-08-23 21:29:59 -0300

    RevengeOS initial Commit

--- revengeos_montana.mk
+++ revengeos_montana.mk
@@ -23,8 +23,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk
 $(call inherit-product, device/motorola/montana/device.mk)
 $(call inherit-product, vendor/motorola/montana/montana-vendor.mk)
 
-# Inherit from the common LineageOS product configuration.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Revenge OS stuff.
+$(call inherit-product, vendor/revengeos/config/common.mk)
+$(call inherit-product, vendor/revengeos/config/gsm.mk)
+IS_PHONE := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_MINIMAL_GAPPS := true
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
@@ -39,7 +43,7 @@ TARGET_SCREEN_HEIGHT := 1920
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 PRODUCT_AAPT_CONFIG := normal
 
-PRODUCT_NAME := lineage_montana
+PRODUCT_NAME := revengeos_montana
 PRODUCT_BRAND := motorola
 PRODUCT_DEVICE := montana
 PRODUCT_MODEL := Moto G5S
@@ -50,5 +54,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="blueline-user 9 PQ3A.190801.002 5670241 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := google/blueline/blueline:9/PQ3A.190801.002/5670241:user/release-keys \
+BUILD_FINGERPRINT := google/blueline/blueline:9/PQ3A.190801.002/5670241:user/release-keys 
 VENDOR_BUILD_FINGERPRINT := google/blueline/blueline:9/PQ3A.190801.002/5670241:user/release-keys
